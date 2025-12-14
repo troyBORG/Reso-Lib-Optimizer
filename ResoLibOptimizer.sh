5b776fcd191bd27b06e552f311c1fa03a1d98e22
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-12-14 00:58:37 -0800

    Basic script setup
    Setup basic bash script that creates a work directory

---
+++ ResoLibOptimizer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ResoDir="$HOME/.steam/steam/steamapps/common/Resonite"
+
+# Setup work dir and remove if existing
+rm -rf /tmp/ResoLibOptimizer
+mkdir /tmp/ResoLibOptimizer
+cd /tmp/ResoLibOptimizer
