388791367333673d72e54dbec12875079085571f
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-12-14 00:59:16 -0800

    Add brotli
    This compiles an optimized build of brotli

--- ResoLibOptimizer.sh
+++ ResoLibOptimizer.sh
@@ -6,3 +6,16 @@ ResoDir="$HOME/.steam/steam/steamapps/common/Resonite"
 rm -rf /tmp/ResoLibOptimizer
 mkdir /tmp/ResoLibOptimizer
 cd /tmp/ResoLibOptimizer
+
+# Clone and compile optimized Brotli
+git clone --depth=1 https://github.com/Yellow-Dog-Man/brotli
+cd brotli
+mkdir out && cd out
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./installed .. -DCMAKE_C_FLAGS="-O3 -march=native" 
+cmake --build . --config Release
+
+# Replace Resonite's Brotli files
+rm "${ResoDir}/brolib_x64.so"
+rm "${ResoDir}/runtimes/linux/native/brolib_x64.so"
+cp "/tmp/ResoLibOptimizer/brotli/out/libbrolib.so" "${ResoDir}/brolib_x64.so"
+cp "/tmp/ResoLibOptimizer/brotli/out/libbrolib.so" "${ResoDir}/runtimes/linux/native/brolib_x64.so"
